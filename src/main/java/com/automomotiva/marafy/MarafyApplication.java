6827889cd891b43288fc1cbf03c6df8f4262b879
Youssef EZZAHRI <yezzahri@menisys.com>
2019-12-14 16:49:27 +0100

    changing package name

--- src/main/java/com/automomotiva/marafy/MarafyApplication.java
+++ src/main/java/com/automomotiva/marafy/MarafyApplication.java
@@ -1,4 +1,4 @@
-package com.automomotiva.marafy.marafy;
+package com.automomotiva.marafy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
