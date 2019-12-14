62a61e7eb33eefcb0e9cc5968fe0a2be05f79548
Youssef EZZAHRI <yezzahri@menisys.com>
2019-12-14 16:15:12 +0100

    Initial commit

---
+++ src/main/java/com/automomotiva/marafy/marafy/MarafyApplication.java
@@ -0,0 +1,13 @@
+package com.automomotiva.marafy.marafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarafyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automomotiva/marafy/marafy/MarafyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automomotiva.marafy.marafy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarafyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
